9e66d93b5f984510f5bb5763ea16c26263553217
Rohit <kumarrohit67476@gmail.com>
2026-06-13 11:21:27 +0530

    Update hello.py

--- hello.py
+++ hello.py
@@ -1,8 +1,4 @@
-import time
-import datetime
+from datetime import datetime
 
 print("DevOps Application Started")
-
-while True:
-    print(f"Application running at {datetime.datetime.now()}")
-    time.sleep(30)
+print(datetime.now())
