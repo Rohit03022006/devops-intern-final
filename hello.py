7a41096bf12c39801c55ee44da271931be475e79
Rohit <kumarrohit67476@gmail.com>
2026-06-14 15:34:23 +0530

    Fix merge conflict and update greeting message

--- hello.py
+++ hello.py
@@ -1,8 +1,5 @@
-<<<<<<< HEAD
-print("Hello, DevOps!")
-=======
 from datetime import datetime
+print("Hello, Devops")
 
 print("DevOps Application Started")
 print(datetime.now())
->>>>>>> 9e66d93b5f984510f5bb5763ea16c26263553217
